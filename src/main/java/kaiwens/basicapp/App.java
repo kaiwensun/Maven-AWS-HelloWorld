06487a838229f8a52ba0c476329b5c18f2f36fad
Kaiwen Sun <skw_kevin@126.com>
2019-09-09 18:29:17 -0700

    try CodeDeploy client

--- src/main/java/kaiwens/basicapp/App.java
+++ src/main/java/kaiwens/basicapp/App.java
@@ -1,6 +1,13 @@
 package kaiwens.basicapp;
 
 import com.amazonaws.client.builder.AwsClientBuilder;
+import com.amazonaws.services.codedeploy.model.AppSpecContent;
+import com.amazonaws.services.codedeploy.model.CreateDeploymentRequest;
+import com.amazonaws.services.codedeploy.AmazonCodeDeploy;
+import com.amazonaws.services.codedeploy.AmazonCodeDeployClientBuilder;
+import com.amazonaws.services.codedeploy.model.RawString;
+import com.amazonaws.services.codedeploy.model.RevisionLocation;
+import com.amazonaws.services.codedeploy.model.RevisionLocationType;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDB;
 import com.amazonaws.services.dynamodbv2.AmazonDynamoDBClientBuilder;
 import com.amazonaws.regions.Regions;
@@ -28,22 +35,58 @@ public class App {
     private List<AmazonS3> clients;
 
     public App() {
-        List<Regions> regions = getRegions();
-        clients = createClients(regions);
+//        List<Regions> regions = getRegions();
+//        clients = createClients(regions);
     }
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        exampleS3Operations();
-
-//        AmazonS3Client s3 = new AmazonS3Client();
-//        s3.setRegion(RegionUtils.getRegion("us-east-2"));
+        exampleCodeDeploy();
 
     }
 
     /*
      * Example labs entry points
      */
+
+    private static void exampleCodeDeploy() {
+        AmazonCodeDeploy codeDeploy = AmazonCodeDeployClientBuilder.standard().build();
+
+        RevisionLocation revisionLocation = new RevisionLocation()
+                .withRevisionType(RevisionLocationType.String)
+                .withString(new RawString().withContent(
+//                .withAppSpecContent(new AppSpecContent().withContent(
+                    "{\n" +
+                    " \t\"version\": 0.0,\n" +
+                    " \t\"Resources\": [{\n" +
+                    " \t\t\"myLambdaFunction\": {\n" +
+                    " \t\t\t\"Type\": \"AWS::Lambda::Function\",\n" +
+                    " \t\t\t\"Properties\": {\n" +
+                    " \t\t\t\t\"Name\": \"my-function\",\n" +
+                    " \t\t\t\t\"Alias\": \"PROD\",\n" +
+                    " \t\t\t\t\"CurrentVersion\": \"1\",\n" +
+                    " \t\t\t\t\"TargetVersion\": \"2\"\n" +
+                    " \t\t\t}\n" +
+                    " \t\t}\n" +
+                    " \t}],\n" +
+                    " \t\"Hooks\": [{\n" +
+                    " \t\t\t\"BeforeAllowTraffic\": \"LambdaFunctionToValidateBeforeTrafficShift\"\n" +
+                    "      },\n" +
+                    "      {\n" +
+                    " \t\t\t\"AfterAllowTraffic\": \"LambdaFunctionToValidateAfterTrafficShift\"\n" +
+                    " \t\t}\n" +
+                    " \t]\n" +
+                    " }"));
+
+        CreateDeploymentRequest request = new CreateDeploymentRequest()
+                .withApplicationName("my-lambda")
+                .withDeploymentGroupName("my-group")
+                .withRevision(revisionLocation);
+
+        codeDeploy.createDeployment(request);
+        codeDeploy.shutdown();
+
+    }
     private static void examplePassSystemPropertyFromPOM() {
         System.out.println(System.getProperty("abc"));
     }
